e8aa287ea8d2b439e05d1c534924192c50a4f2a3
Sachman19 <sachindmishra@knights.ucf.edu>
2021-06-06 02:05:02 -0400

    Version 1 Complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 5 Solution
+ *  Copyright 2021 Sachin Mishra
+ */
+
 package com.company;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter a number. ");
+        int num1 = input.nextInt();
+
+        System.out.print("Please enter another number. ");
+        int num2 = input.nextInt();
+
+        String outAdd = num1 + " + " + num2 + " = " + (num1 + num2);
+        String outSub = num1 + " - " + num2 + " = " + (num1 - num2);
+        String outMul = num1 + " * " + num2 + " = " + (num1 * num2);
+        String outDiv = num1 + " / " + num2 + " = " + (num1 / num2);
+
+        System.out.printf("%s%n%s%n%s%n%s%n", outAdd, outSub, outMul, outDiv);
     }
 }
